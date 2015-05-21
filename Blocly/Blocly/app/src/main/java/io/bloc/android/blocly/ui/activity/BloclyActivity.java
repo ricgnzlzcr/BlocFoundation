6d12243e305e9a7b579cfb77c8b811b6930b4051
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-05-21 17:35:37 -0400

    Checkpoint 33 completed

--- Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,9 +1,18 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import io.bloc.android.blocly.R;
 
 /**
  * Created by ricardo on 5/18/15.
  */
 public class BloclyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blocly);
+    }
 }
