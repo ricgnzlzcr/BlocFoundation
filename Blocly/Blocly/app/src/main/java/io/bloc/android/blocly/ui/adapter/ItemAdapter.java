963d79b1258a2d35ded73bc93ace60d34d01fe2e
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-06-16 13:34:24 -0400

    Completed checkpoint 41

--- Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -52,6 +52,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
     // #9
     class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener, View.OnClickListener, CompoundButton.OnCheckedChangeListener {
 
+        boolean contentExpanded;
         TextView title;
         TextView feed;
         TextView content;
@@ -60,6 +61,9 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
         ImageView headerImage;
         CheckBox archiveCheckbox;
         CheckBox favoriteCheckbox;
+        View expandedContentWrapper;
+        TextView expandedContent;
+        TextView visitSite;
         // #6
         RssItem rssItem;;
 
@@ -73,8 +77,12 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
             headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
             archiveCheckbox = (CheckBox) itemView.findViewById(R.id.cb_rss_item_check_mark);
             favoriteCheckbox = (CheckBox) itemView.findViewById(R.id.cb_rss_item_favorite_star);
+            expandedContentWrapper = itemView.findViewById(R.id.ll_rss_item_expanded_content_wrapper);
+            expandedContent = (TextView) expandedContentWrapper.findViewById(R.id.tv_rss_item_content_full);
+            visitSite = (TextView) expandedContentWrapper.findViewById(R.id.tv_rss_item_visit_site);
 
             itemView.setOnClickListener(this);
+            visitSite.setOnClickListener(this);
             archiveCheckbox.setOnCheckedChangeListener(this);
             favoriteCheckbox.setOnCheckedChangeListener(this);
         }
@@ -84,6 +92,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
             feed.setText(rssFeed.getTitle());
             title.setText(rssItem.getTitle());
             content.setText(rssItem.getDescription());
+            expandedContent.setText(rssItem.getDescription());
             if (rssItem.getImageUrl() != null) {
                 // #8
                 headerWrapper.setVisibility(View.VISIBLE);
@@ -129,7 +138,13 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterVie
         // #7
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), rssItem.getTitle(), Toast.LENGTH_SHORT).show();
+            if (view == itemView) {
+                contentExpanded = !contentExpanded;
+                expandedContentWrapper.setVisibility(contentExpanded ? View.VISIBLE : View.GONE);
+                content.setVisibility(contentExpanded ? View.GONE : View.VISIBLE);
+            } else {
+                Toast.makeText(view.getContext(), "Visit " + rssItem.getUrl(), Toast.LENGTH_SHORT).show();
+            }
         }
 
         /*
