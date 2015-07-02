bf3570dc6b569666cbf3f0c1a57d6bcf7e8e8790
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-07-02 11:44:41 -0400

    Completed checkpoint 50

---
+++ Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/api/network/NetworkRequest.java
@@ -0,0 +1,50 @@
+package io.bloc.android.blocly.api.network;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by ricardo on 7/2/15.
+ */
+public abstract class NetworkRequest<Result> {
+
+    // #2
+    public static final int ERROR_IO = 1;
+    public static final int ERROR_MALFORMED_URL = 2;
+
+    private int errorCode;
+
+    protected void setErrorCode(int errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+    // #3
+    public abstract Result performRequest();
+
+    protected InputStream openStream(String urlString) {
+        URL url = null;
+        try {
+            url = new URL(urlString);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            setErrorCode(ERROR_MALFORMED_URL);
+            return null;
+        }
+        InputStream inputStream = null;
+        try {
+            // #5
+            inputStream = url.openStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            setErrorCode(ERROR_IO);
+            return null;
+        }
+        return inputStream;
+    }
+}
