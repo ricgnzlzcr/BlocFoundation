2c06646fe67d7b1db78f5a994778d7a7d7f46b97
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-07-02 12:48:31 -0400

    Completed checkpoint 50 assignment

--- Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
+++ Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/api/network/GetFeedsNetworkRequest.java
@@ -13,7 +13,7 @@ import java.io.InputStreamReader;
 public class GetFeedsNetworkRequest extends NetworkRequest {
 
     // #6
-    String [] feedUrls;
+    String[] feedUrls;
 
     public GetFeedsNetworkRequest(String... feedUrls) {
         this.feedUrls = feedUrls;
@@ -34,6 +34,11 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
                 while (line != null) {
                     Log.v(getClass().getSimpleName(), "Line: " + line);
                     line = bufferedReader.readLine();
+
+                    //Ric's code
+                    int count = getRSSFeedCount(line);
+                    Log.d("Feed Count", "" + count);
+
                 }
                 // #9
                 bufferedReader.close();
@@ -45,4 +50,27 @@ public class GetFeedsNetworkRequest extends NetworkRequest {
         }
         return null;
     }
+
+    //Ric's methods
+    private int getRSSFeedCount(String line) {
+        String title = "<title>";
+        int rssFeedCount = 0;
+        rssFeedCount = instancesOfStringInString(title, line, rssFeedCount);
+        return rssFeedCount;
+    }
+
+    private int instancesOfStringInString(String wordToFind, String stringToSearch, int count) {
+        int wordToFindLength = wordToFind.length();
+        if (stringToSearch != null) {
+            if (stringToSearch.indexOf(wordToFind) == -1) {
+                return count;
+            } else {
+                int posOfTitle = stringToSearch.indexOf(wordToFind);
+                return instancesOfStringInString(wordToFind, stringToSearch.substring(posOfTitle + wordToFindLength), count + 1);
+            }
+        }
+        return count;
+    }
+
+
 }
