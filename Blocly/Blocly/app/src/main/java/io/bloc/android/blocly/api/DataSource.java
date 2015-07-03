2eb6c4625e91b33e5bc3674bb348af8bf78236c3
Ricardo Gonzalez <ricgnzlzcr@gmail.com>
2015-07-02 21:10:30 -0400

    Completed checkpoint 51 assignment

--- Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ Blocly/Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -1,5 +1,8 @@
 package io.bloc.android.blocly.api;
 
+import android.content.Intent;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,20 +17,29 @@ import io.bloc.android.blocly.api.network.GetFeedsNetworkRequest;
  */
 public class DataSource {
 
+    public static final String ACTION_DOWNLOAD_COMPLETED = DataSource.class.getCanonicalName().concat(".ACTION_DOWNLOAD_COMPLETED");
+
     private List<RssFeed> feeds;
     private List<RssItem> items;
+    private List<GetFeedsNetworkRequest.FeedResponse> dataList;
 
     public DataSource() {
         feeds = new ArrayList<RssFeed>();
         items = new ArrayList<RssItem>();
-        createFakeData();
+        //createFakeData();
+        //createData();
 
         new Thread(new Runnable() {
             @Override
             public void run() {
-                new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+                dataList = new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+                Log.d("DATASOURCE", "Datalist created!");
+                createData();
+                BloclyApplication.getSharedInstance().sendBroadcast(new Intent(ACTION_DOWNLOAD_COMPLETED));
             }
         }).start();
+
+
     }
 
     public List<RssFeed> getFeeds() {
@@ -49,6 +61,25 @@ public class DataSource {
                     "http://favoritefeed.net?story_id=an-incredible-news-story",
                     "http://rs1img.memecdn.com/silly-dog_o_511213.jpg",
                     0, System.currentTimeMillis(), false, false, false));
+        }
+    }
+
+    private void createData() {
+        if (dataList != null) {
+            for (GetFeedsNetworkRequest.FeedResponse feed:dataList) {
+                feeds.add(new RssFeed(feed.channelTitle, feed.channelDescription, feed.channelURL, feed.channelFeedURL));
+                for (int i = 0; i < feed.channelItems.size(); i++) {
+                    items.add(new RssItem((String.valueOf(i)),
+                            feed.channelItems.get(i).itemTitle,
+                            feed.channelItems.get(i).itemDescription,
+                            "http://favoritefeed.net?story_id=an-incredible-news-story",
+                            "http://rs1img.memecdn.com/silly-dog_o_511213.jpg",
+                            0, System.currentTimeMillis(), false, false, false
+                            ));
+                    Log.d("ITEM TITLE", feed.channelItems.get(i).itemTitle);
+                }
+                Log.d("FEED NAME", feed.channelTitle);
+            }
         }
     }
 
